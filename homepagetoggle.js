3e5128692c0909e020e37480490366cb5328e3a0
autosourcemotors <33140342+autosourcemotors@users.noreply.github.com>
2018-05-07 10:13:50 -0600

    Update homepagetoggle.js

--- homepagetoggle.js
+++ homepagetoggle.js
@@ -1,6 +1,9 @@
 window.onload = function(){
   document.getElementById('customSalesEventRow').className = 'expanded';
   document.getElementById('customSalesEventRow').className = 'override';
+  document.getElementById('customSalesEventRow').className = 'override';
+  $('.expanded-row.darken-overlay.container-wide').attr('width', 'width: 100vw !important');
+  $( ".expanded-row.darken-overlay.container-wide" ).css( "maxWidth", '100vw');
   $(".expanded-row-title").remove();
   $(".collapsed-row").remove();
   $(".expanded-toggle").remove();
