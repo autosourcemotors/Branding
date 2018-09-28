30325d3aa09a5af479f67718f418d3cbce353fe5
autosourcemotors <33140342+autosourcemotors@users.noreply.github.com>
2018-09-28 17:15:28 -0600

    Create VideosPage.js

---
+++ VideosPage.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    $('.Slider').slick({
+        draggable: true,
+        swipe: true,
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 200,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        variableWidth: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
+(function () {
+    var youtube = document.querySelectorAll(".youtube");
+    for (var i = 0; i < youtube.length; i++) {
+        var source = "https://img.youtube.com/vi/" + youtube[i].dataset.embed + "/sddefault.jpg";
+        var image = new Image();
+        image.src = source;
+        image.addEventListener("load", function () {
+            youtube[i].appendChild(image);
+        }(i));
+        youtube[i].addEventListener("click", function () {
+            var iframe = document.createElement("iframe");
+            iframe.setAttribute("frameborder", "0");
+            iframe.setAttribute("allowfullscreen", "");
+            iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.embed +
+                "?rel=0&showinfo=0&autoplay=1");
+            this.innerHTML = "";
+            this.appendChild(iframe);
+        });
+    };
+})();
